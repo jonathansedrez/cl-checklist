d0b6233c6f388239df8a78d50add923c6c5e4201
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-02-05 22:43:14 -0300

    feat: add routes based on service position

---
+++ pages/recepcao/index.tsx
@@ -0,0 +1,33 @@
+export default function ManagerPage() {
+  return (
+    <fieldset className="m-5">
+      <div className="space-y-2">
+        <p>Pré recepição</p>
+        <div>
+          <p>todo</p>
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <p>Recepição</p>
+        <div>
+          <p>todo</p>
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <p>Durante o culto</p>
+        <div>
+          <p>todo</p>
+        </div>
+      </div>
+
+      <div className="space-y-2">
+        <p>Pós culto</p>
+        <div>
+          <p>todo</p>
+        </div>
+      </div>
+    </fieldset>
+  );
+}
