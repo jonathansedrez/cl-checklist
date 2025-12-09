b70a2e38e91c2c8da012a466c80e449f933e9b2b
Jonathan Sedrez <jonathansedrez@gmail.com>
2025-12-08 21:39:01 -0300

    feat: add manager page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <Link href="/checklist?page=CAFE">Café</Link>
       <Link href="/checklist?page=APOIO">Apoio</Link>
+      <Link href="/checklist?page=APOIO">Manager</Link>
     </div>
   );
 }
